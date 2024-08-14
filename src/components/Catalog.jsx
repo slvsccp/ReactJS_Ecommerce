e0bd76171f1a8674a2a0092a971b18cdb3f81452
Matheus Silva <teus_26@outlook.com>
2024-08-14 16:56:17 -0300

    ajustando itens do catalogo

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -6,7 +6,7 @@ function Catalog() {
   return (
     <div>
       <h1>Catálogo de Produtos</h1>
-      <div className="products-container">
+      <div className="product-container">
         {
           productsData.map((product) => (
             <Product key={product.id} product={product} />
